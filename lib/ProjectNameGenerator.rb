28cf4e0046c1d5fb3808aed9af624fcea1b11e21
Whatisinternet <joshteeter@gmail.com>
2015-02-26 23:55:24 -0500

    Updated algo to provide a better name

--- lib/ProjectNameGenerator.rb
+++ lib/ProjectNameGenerator.rb
@@ -4,7 +4,10 @@ require 'faker'
 module ProjectNameGenerator
   class Crap
     def get_project_name
-      "#{Faker::Commerce.product_name.split(' ')[0]}-#{Faker::App.name}".downcase!
+      part1 = "#{Faker::Commerce.product_name.split(' ')[0]}"
+      part2 = "#{Faker::Hacker.noun}"
+      part3 = "#{Faker::App.name}"
+      "#{part1}-#{part2}-#{part3}".downcase.gsub(" ", "-")
     end
   end
 end
